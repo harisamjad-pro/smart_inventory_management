26e65a7cf500df1515ee43ae5cd19a6f758eaac5
harisamjad-pro <harisamjad56@gmail.com>
2025-08-19 03:29:48 +0500

    Install Inter Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const interFont = localFont({
+  variable: "--font-inter-sans",
+  src: [
+    { path: "./fonts/Inter_Thin.ttf", weight: "100" },
+    { path: "./fonts/Inter_ExtraLight.ttf", weight: "200" },
+    { path: "./fonts/Inter_Light.ttf", weight: "300" },
+    { path: "./fonts/Inter_Regular.ttf", weight: "400" },
+    { path: "./fonts/Inter_Medium.ttf", weight: "500" },
+    { path: "./fonts/Inter_SemiBold.ttf", weight: "600" },
+    { path: "./fonts/Inter_Bold.ttf", weight: "700" },
+    { path: "./fonts/Inter_ExtraBold.ttf", weight: "800" },
+    { path: "./fonts/Inter_Black.ttf", weight: "900" },
+  ]
 });
 
 export const metadata: Metadata = {
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interFont.variable} antialiased`}
       >
         {children}
       </body>
