bbad9ff5dc72d852685a501471284f37763dfef4
Haris Amjad <harisamjad56@gmail.com>
2025-08-27 14:21:42 +0500

    new fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Sidebar from "@/components/layout/Sidebar";
 
 const interFont = localFont({
   variable: "--font-inter-sans",
